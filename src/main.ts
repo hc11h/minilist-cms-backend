b861786351828eb669298e03dfc3264f2a7a31c0
hch-prog <chaniyara.harsh26@gmail.com>
2025-10-31 21:50:04 +0530

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import cookieParser from 'cookie-parser';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,8 +19,12 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3001;
 
+  app.getHttpAdapter().getInstance().get('', (req: Request, res: Response) => {
+    res.status(200).json({ status: 'Backend is working!' });
+  });
+
   await app.listen(port);
-  console.log(`🚀 Application is running on: http://localhost:${port}`);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 
 bootstrap().catch((err) => {
